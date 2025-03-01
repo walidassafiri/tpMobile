c3c278ac0887b05e8d113078009911cabb53f3ee
Arthur <95504979+arvllgr@users.noreply.github.com>
2025-03-01 16:49:15 +0100

    fix: Config conteneurs Screen

--- app/src/main/java/com/example/tpmobile/ui/screens/ConfigurerConteneursScreen.kt
+++ app/src/main/java/com/example/tpmobile/ui/screens/ConfigurerConteneursScreen.kt
@@ -12,8 +12,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
